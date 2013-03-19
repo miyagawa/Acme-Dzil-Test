c5a9af3fa6c240bad6a3d0c6e51cf5a49b4e3a48
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2013-03-19 11:15:59 -0700

    initial commit

---
+++ lib/Acme/Dzil/Test.pm
@@ -0,0 +1,41 @@
+package Acme::Dzil::Test;
+
+use strict;
+use 5.008_001;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+Acme::Dzil::Test -
+
+=head1 SYNOPSIS
+
+  use Acme::Dzil::Test;
+
+=head1 DESCRIPTION
+
+Acme::Dzil::Test is
+
+=head1 AUTHOR
+
+Tatsuhiko Miyagawa E<lt>miyagawa@bulknews.netE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013- Tatsuhiko Miyagawa
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
